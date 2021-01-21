dd18de5d6cc84f91ae5a3a2c5472c858d96c5f4c
Rodrigo <tazzsoni@gmail.com>
2021-01-21 11:35:46 -0300

    expiration time to one minute

--- src/main/java/com/gerenciador/apigerenciador/service/TokenAuthenticationService.java
+++ src/main/java/com/gerenciador/apigerenciador/service/TokenAuthenticationService.java
@@ -12,7 +12,7 @@ import java.util.Date;
 
 public class TokenAuthenticationService {
 
-    private static final long EXPIRATIONTIME = 864000000;
+    private static final long EXPIRATIONTIME = 60000;
     private static final String SECRET = "MySecreteApp";
     private static final String TOKEN_PREFIX = "Bearer";
     private static final String HEADER_STRING = "Authorization";
